7e9bef199637cc97afc13950d340f11e8f137e88
Trey Beauregard <trey.beauregard.ctr@nrl.navy.mil>
2015-12-04 12:34:33 -0500

    font weight increased

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
     <head>
         <title>Lunch - @yield('title')</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
 
         <style>
@@ -12,7 +12,7 @@
                 padding: 0;
                 width: 100%;
                 display: table;
-                font-weight: 100;
+                font-weight: 300;
                 font-family: 'Lato';
             }
 
